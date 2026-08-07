3fdb8d7e55b4ad8ee8d6c09f7d5c98b0eb292efc
andrewBristowx <138131314+andrewBristowx@users.noreply.github.com>
2026-08-06 20:27:57 -0500

    Validar temporalmente ArlightBingo 1.48.52

--- src/main/java/com/arlight/bingo/game/GameState.java
+++
@@ -1,9 +0,0 @@
-package com.arlight.bingo.game;
-
-public enum GameState {
-    WAITING,
-    COUNTDOWN,
-    RUNNING,
-    TIEBREAK,
-    ENDED
-}
--- src/main/java/com/arlight/bingo/game/WinCondition.java
+++
@@ -1,8 +0,0 @@
-package com.arlight.bingo.game;
-
-public enum WinCondition {
-    POINTS,   // gana quien tenga mas casillas completadas cuando se acaba el tiempo (1 punto por casilla)
-    LINE,
-    FULL_CARD,
-    BLACKOUT // alias de FULL_CARD, incluido por claridad de nombre para los admins
-}
